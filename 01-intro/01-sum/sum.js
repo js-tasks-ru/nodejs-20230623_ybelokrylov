a5099d109f1f2cebc06c820ca86b67f2c667ca5d
YBelokrylov <YBelokrylov@ibs.ru>
2023-06-27 12:52:40 +0400

    add solution for module 01-intro task 01-sum

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,11 @@
 function sum(a, b) {
-  /* ваш код */
+  if ([a, b].some((value) => typeof value !== 'number')) {
+    throw new TypeError(
+        'Аргументы функции должны иметь числовой тип данных.',
+    );
+  }
+
+  return a + b;
 }
 
 module.exports = sum;
